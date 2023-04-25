a0da0386a6e76bf86f94744b78498202d738b253
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2023-04-25 11:02:18 +0100

    Task 5

--- students.php
+++ students.php
@@ -1,34 +1,37 @@
 <?php
-    include("_includes/config.inc");
-    include("_includes/dbconnect.inc");
-    include("_includes/functions.inc");
 
-   // check if logged in
-    if (isset($_SESSION['id'])) {
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
 
-        echo template("templates/partials/header.php");
-        echo template("templates/partials/nav.php");
+// check if logged in
+if (isset($_SESSION['id'])) 
+{
+  echo template("templates/partials/header.php");
+  echo template("templates/partials/nav.php");
 
-      // Build SQL statment that selects a student's modules
-        $sql = "SELECT * FROM student";
+  // Build SQL statment that selects a student's modules
+  $sql = "SELECT * FROM student";
 
-        $result = mysqli_query($conn,$sql);
+  $result = mysqli_query($conn,$sql);
 
-      // Create a form
-        $data['content'] .= "<form action='deletestudents.php' method='POST'>";
+  // Create form
+  $data['content'] .= "<form action='deletestudents.php' method='POST'>";
 
-      // Prepare page content
-        $data['content'] .= "<table border='1'>";
+  // Prepare page content
+  $data['content'] .= "<table class='table table-bordered border-dark'>";
 
-      // Display table headings  
-        $data['content'] .= "<tr><th>Student ID</th><th>First Name</th>
-                            <th>Last Name</th><th>DOB</th>
-                            <th>House</th><th>Town</th>
-                            <th>County</th><th>Country</th>
-                            <th>Postcode</th><th>Profile Image</th></tr>";
+  // Display table headings  
+  $data['content'] .= "<tr><th>Student ID</th><th>First Name</th>
+                      <th>Last Name</th><th>DOB</th>
+                      <th>House</th><th>Town</th>
+                      <th>County</th><th>Country</th>
+                      <th>Postcode</th><th>Profile Image</th>
+                      <th>X</th></tr>";
 
-      // Display students within the html table
-        while($row = mysqli_fetch_array($result)) {
+      // Display students within html table
+        while($row = mysqli_fetch_array($result)) 
+        {
         $data['content'] .= "<tr>";
             $data['content'] .= "<td> {$row["studentid"]} </td>";
             $data['content'] .= "<td> {$row["firstname"]} </td>";
@@ -39,24 +42,32 @@
             $data['content'] .= "<td> {$row["county"]} </td>";
             $data['content'] .= "<td> {$row["country"]} </td>";
             $data['content'] .= "<td> {$row["postcode"]} </td>";
-            $data['content'] .= "<td> {$row["profile_picture"]} </td>";
+            if (!empty($row["profile_picture"])) 
+            {
+              $data['content'] .= "<td class='text-center'> <img src='data:image/jpeg;base64," . base64_encode($row['profile_picture']) . "' height='100' width='80' /> </td>";
+            } 
+            else 
+            {
+              $data['content'] .= "<td> No Image Available </td>";
+            }
             $data['content'] .= "<td> <input type='checkbox' name='students[]' value='$row[studentid]' ></td>";
             $data['content'] .= "</tr>";
         }
         $data['content'] .= "</table><br>";
 
       // Delete button  
-        $data['content'] .= "<input type='submit' name='deletebtn' value='Delete'/>";
+      $data['content'] .= "<input type='submit' class='btn btn-outline-danger' name='deletebtn' value='Delete'/>";
 
       // Close form  
-        $data['content'] .= "</form>";
+      $data['content'] .= "</form>";
 
-      // render template
-        echo template("templates/default.php", $data);
+      // Render template
+      echo template("templates/default.php", $data);
     } 
     else 
     {
-        header("Location: index.php");
+      header("Location: index.php");
     }
+
     echo template("templates/partials/footer.php");
 ?>
