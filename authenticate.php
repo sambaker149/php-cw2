7294d9c024462969d44e3fc9e221ca496e4bb6ef
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2023-05-02 10:38:37 +0100

    Implement Faker

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,4 @@
 <?php
-
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
@@ -7,12 +6,15 @@
    // If the student has already been authenticated the $_SESSION['id'] variable
    // will been assigned their student id.
 
-
    // redirect to index if $_POST values not set or $_SESSION['id'] is already set
-   if (!isset($_POST['txtid']) || !isset($_POST['txtpwd']) || isset($_SESSION['id'])) {
+   if (!isset($_POST['txtid']) || !isset($_POST['txtpwd']) || isset($_SESSION['id'])) 
+   {
       header("Location: index.php");
-	} else {
-      if (validatelogin($_POST['txtid'],$_POST['txtpwd']) == true) {
+	} 
+   else 
+   {
+      if (validatelogin($_POST['txtid'],$_POST['txtpwd']) == true) 
+      {
          // valid
          header("Location: index.php?return=success");
       } else {
--- deletestudents.php
+++ deletestudents.php
@@ -3,16 +3,17 @@
     include("_includes/dbconnect.inc");
     include("_includes/functions.inc");
 
-   // check if logged in
+   // Check if logged in
     if (isset($_SESSION['id'])) 
     {
 
         if(isset($_POST['confirm_delete']) && !empty($_POST['students'])) 
         {
+
             // Loop over students and delete entries  
             foreach($_POST['students'] as $student_id) 
             {
-                //Execute sql statement to delete items  
+                //Execute SQL statement to delete items  
                 $sql = "DELETE FROM student WHERE studentid = '$student_id'";
                 // Run query  
                 $result = mysqli_query($conn,$sql);
@@ -22,8 +23,8 @@
         } 
         else 
         {
-            // Show the confirmation message
-            $confirm_message = "Are you sure you want to delete?";
+            // Display confirmation message
+            $confirm_message = "<div><h1 class='text-center'>Are you sure you want to delete?</h1>";
             $confirm_message .= "<br><br>";
             $confirm_message .= "<form method='POST'>";
             if (!empty($_POST['students'])) 
@@ -32,10 +33,10 @@
                 {
                     $confirm_message .= "<input type='hidden' name='students[]' value='$student_id'>";
                 }
-                $confirm_message .= "<input type='submit' name='confirm_delete' value='Yes' style='margin-right: 10px;'>";
+                $confirm_message .= "<input type='submit' class='btn btn-outline-danger' name='confirm_delete' value='Yes' style='margin-right: 10px;'>";
             }
-            $confirm_message .= "<input type='button' value='No' onclick='window.location.href=\"students.php\"'>";
-            $confirm_message .= "</form>";
+            $confirm_message .= "<input type='button' class='btn btn-outline-danger' value='No' onclick='window.location.href=\"students.php\"'>";
+            $confirm_message .= "</form></div>";
 
             echo $confirm_message;
         }
@@ -47,10 +48,11 @@
         header("Location: index.php");
     }
 ?>
+
 <script>
     function showConfirm() 
     {
-        confirm_message = "Are you sure you want to delete?";
+        confirm_message = "Are you sure you want to delete this Student?";
         if(confirm(confirm_message)) 
         {
             document.forms[0].submit();
